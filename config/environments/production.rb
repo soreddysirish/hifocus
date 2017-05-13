398893d126e15ea1d4a7bdcb09e30c7590a6ab12
soreddysirish <ssireeshreddy@gmail.com>
2017-05-13 19:07:44 +0530

    application filedsfgs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -76,6 +76,7 @@ end
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.js_compressor = Uglifier.new(mangle: false)
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
