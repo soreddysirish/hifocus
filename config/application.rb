9dc3062568b3ce95a639f4166666060a634013ec
soreddysirish <ssireeshreddy@gmail.com>
2017-05-13 23:12:09 +0530

    asset precomple

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Hifocusgate
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << "#{Rails.root}/app/assets/templates"
      config.assets.paths << "#{Rails.root}/app/assets/fonts"
+     config.assets.paths << Rails.root.join("app", "assets", "images")
     # config.serve_static_files = true
     # config.public_file_server.enabled =true
     config.serve_static_assets = true
