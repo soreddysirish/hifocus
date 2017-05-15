59c8d21b8e71bb6d4bc9c4eb56e118b5d046ac00
soreddysirish <ssireeshreddy@gmail.com>
2017-05-15 17:06:18 +0530

    added mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ end
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   # config.assets.js_compressor = Uglifier.new(mangle: false)
-
+config.action_mailer.perform_deliveries = true
 config.action_mailer.default_url_options = { host:"hifocusgate.herokuapp.com" }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
